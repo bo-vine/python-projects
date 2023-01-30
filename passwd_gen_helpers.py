06924211097f6987c607d1f7f88e863488d5797a
tylermav <tylermavan@gmail.com>
2023-01-29 19:49:54 -0600

    Commit all files

---
+++ passwd_gen_helpers.py
@@ -0,0 +1,39 @@
+"""
+This software is used to generate a very simple password with modern criterias for a strong password
+- Makes saying the password over the phone easier
+- then spelling everything out
+"""
+
+import random as r
+
+# Words used to randomize the passwords
+
+
+#func to randomly join two words from list
+def generate_words():
+    words = [
+              "circle", "triangle", "square", "rectangle", "star" , # Shapes
+              "green", "blue", "red", "yellow", "purple", "orange", # Colors
+              "dog", "cat", "fish", "bird"                          # Animals
+            ]
+    chosen_words = r.sample(words, 2)
+
+    return(chosen_words)
+
+
+#func to generate two digits 
+def generate_num():
+    chosen_number = r.randint(10, 99)
+    
+    return(chosen_number)
+
+def pick_random_symbol():
+    symbols=["!", "@", "#", "$", "%"]
+    chosen_symbol=r.choice(symbols)
+
+    return symbol
+
+
+#func to print password
+def print_password(words, numbers):
+    return("print_password() End")
---
+++ run.py
@@ -0,0 +1,7 @@
+import passwd_gen_helpers as pg
+
+def main():
+    x = pg.generate_num()
+
+if __name__ == "__main__":
+    main()
